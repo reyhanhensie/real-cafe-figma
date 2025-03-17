b9a01200eb97917a751ddb3b9302d2371da2dd44
reyhanhensie <reyhanhensie@gmail.com>
2025-03-17 13:09:16 +0700

    setup vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return (
+        <div>
+            <h1>Hello from React inside Laravel!</h1>
+        </div>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
-// vite.config.js
-
 import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'public/build',
-    manifest: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'resources/js/app.jsx'),
-      },
-    },
-  },
+    plugins: [
+        laravel({
+            input: ['resources/js/app.jsx'],
+            refresh: true,
+        }),
+        react(),
+    ],
 });
